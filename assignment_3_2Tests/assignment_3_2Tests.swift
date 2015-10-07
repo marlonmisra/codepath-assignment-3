955f99d9d989e06c8ee9b0f310e95d2137f0ae44
Marlon <marlonmisra@gmail.com>
2015-10-06 22:17:29 -0700

    Initial Commit

---
+++ assignment_3_2Tests/assignment_3_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  assignment_3_2Tests.swift
+//  assignment_3_2Tests
+//
+//  Created by Marlon Misra on 2015-10-06.
+//  Copyright © 2015 Marlon Misra. All rights reserved.
+//
+
+import XCTest
+@testable import assignment_3_2
+
+class assignment_3_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
